4df78ba51558d2cbe401119860dead7a91510de7
Tudor Zaharia <zahariatm@yahoo.com>
2023-09-12 22:35:27 +0300

    Created fileInput package and classes.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 
-public class Main {
-    public static void main(String[] args) {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import fileInput.Input;
 
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Input input = objectMapper.readValue(new File(args[0]), Input.class);
+
+        ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
+        objectWriter.writeValue(new File(args[1]), input);
     }
 }
